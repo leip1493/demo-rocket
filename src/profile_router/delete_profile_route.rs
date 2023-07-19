86475ecd75361736f339bfdc36baf421bb120278
Luis <leip1493@gmail.com>
2023-07-19 09:31:39 -0400

    json con http status en delete profile

--- src/profile_router/delete_profile_route.rs
+++ src/profile_router/delete_profile_route.rs
@@ -1,28 +1,42 @@
 use crate::database::DB;
 use entity::profile;
+use rocket::http::Status;
+use rocket::response::status;
 use rocket::serde::json::{json, Value};
 use sea_orm::{EntityTrait, ModelTrait};
 use sea_orm_rocket::Connection;
 
 #[delete("/<id>")]
-pub async fn run(connection: Connection<'_, DB>, id: i32) -> Value {
+pub async fn run(connection: Connection<'_, DB>, id: i32) -> status::Custom<Value> {
     let db = connection.into_inner();
 
     let result_db_profile = profile::Entity::find_by_id(id).one(db).await;
     if let Err(error) = result_db_profile {
-        return json!({ "error": error.to_string() });
+        return status::Custom(
+            Status::InternalServerError,
+            json!({ "error": error.to_string() }),
+        );
     }
 
     let option_db_profile = result_db_profile.unwrap();
     if let None = option_db_profile {
-        return json!({ "error": format!("Profile with id {} not found", id) });
+        return status::Custom(
+            Status::NotFound,
+            json!({ "error": format!("Profile with id {} not found", id) }),
+        );
     }
 
     let db_profile = option_db_profile.unwrap();
-    
+
     if let Err(error) = db_profile.delete(db).await {
-        return json!({ "error": error.to_string() });
+        return status::Custom(
+            Status::InternalServerError,
+            json!({ "error": error.to_string() }),
+        );
     }
 
-    json!({ "message": format!("Profile with id {} deleted", id)})
+    status::Custom(
+        Status::Ok,
+        json!({ "message": format!("Profile with id {} deleted", id)}),
+    )
 }
