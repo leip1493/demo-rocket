163804b755c2e44b751b2a6e2dfc32bc552c6b97
Luis <leip1493@gmail.com>
2023-07-19 00:26:27 -0400

    se crea ruta para eliminar profile por id

--- src/profile_router/delete_profile_route.rs
+++ src/profile_router/delete_profile_route.rs
@@ -1,4 +1,28 @@
-#[delete("/")]
-pub(crate) fn run() -> &'static str {
-    "delete profile"
+use crate::database::DB;
+use entity::profile;
+use rocket::serde::json::{json, Value};
+use sea_orm::{EntityTrait, ModelTrait};
+use sea_orm_rocket::Connection;
+
+#[delete("/<id>")]
+pub async fn run(connection: Connection<'_, DB>, id: i32) -> Value {
+    let db = connection.into_inner();
+
+    let result_db_profile = profile::Entity::find_by_id(id).one(db).await;
+    if let Err(error) = result_db_profile {
+        return json!({ "error": error.to_string() });
+    }
+
+    let option_db_profile = result_db_profile.unwrap();
+    if let None = option_db_profile {
+        return json!({ "error": format!("Profile with id {} not found", id) });
+    }
+
+    let db_profile = option_db_profile.unwrap();
+    
+    if let Err(error) = db_profile.delete(db).await {
+        return json!({ "error": error.to_string() });
+    }
+
+    json!({ "message": format!("Profile with id {} deleted", id)})
 }
