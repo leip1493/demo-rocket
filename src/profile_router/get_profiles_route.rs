b073de310a652e905036d1dd324143d1bc2f1aa7
Luis <leip1493@gmail.com>
2023-07-22 14:12:05 -0400

    mapeo de rutas get

--- src/profile_router/get_profiles_route.rs
+++ src/profile_router/get_profiles_route.rs
@@ -1,10 +1,19 @@
 use crate::database::DB;
 use entity::profile;
 use rocket::serde::json::{json, Value};
-use sea_orm::{EntityTrait, QueryOrder};
+use sea_orm::{prelude::DateTimeUtc, EntityTrait, QueryOrder};
 use sea_orm_rocket::Connection;
+use serde::Serialize;
 
-#[get("/")]
+#[derive(Serialize)]
+struct MappedGetProfiles {
+    id: i32,
+    name: String,
+    email: String,
+    created_at: DateTimeUtc,
+}
+
+#[get("/", format = "json")]
 pub async fn run(connection: Connection<'_, DB>) -> Value {
     let db = connection.into_inner();
 
@@ -18,5 +27,15 @@ pub async fn run(connection: Connection<'_, DB>) -> Value {
         Err(_) => Vec::new(),
     };
 
-    json!({ "profiles": profiles })
+    let mapped_profiles = profiles
+        .iter()
+        .map(|profile| MappedGetProfiles {
+            id: profile.id,
+            name: profile.name.to_owned(),
+            email: profile.email.to_owned(),
+            created_at: profile.created_at.to_owned(),
+        })
+        .collect::<Vec<MappedGetProfiles>>();
+
+    json!({ "profiles": mapped_profiles })
 }
